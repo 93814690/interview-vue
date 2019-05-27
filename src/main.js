7c2dd6e95fb44d93727736455e8f0f3b32a524f6
93814690 <93814690@qq.com>
2019-05-27 17:49:25 +0800

    add: 引入quill的css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import api from './http/index'
 import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.config.productionTip = false;
 
