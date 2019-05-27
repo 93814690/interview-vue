74aeb2b0426dee91faf0d759e238b757a148a0cb
93814690 <93814690@qq.com>
2019-05-27 12:26:30 +0800

    使用vue-quill-editor

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import api from './http/index'
+import VueQuillEditor from 'vue-quill-editor'
 
 Vue.config.productionTip = false;
 
-
 Vue.use(ElementUI);
 Vue.use(api);
-
+Vue.use(VueQuillEditor);
 
 new Vue({
   router,
